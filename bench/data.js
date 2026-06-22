98dc5c9d5b4dce7f9dd1f6943e3608910bea61ba
github-action-benchmark <github@users.noreply.github.com>
2026-06-22 08:57:55 +0000

    add gent throughput (customBiggerIsBetter) benchmark result for 69337debc72ec4acc6845eb83540e2c45e55350c

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782054051519,
+  "lastUpdate": 1782118675888,
   "repoUrl": "https://github.com/stepan662/gent-go",
   "entries": {
     "gent throughput": [
@@ -946,6 +946,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "spawn recursive postgres",
             "value": 483,
+            "unit": "inst/s",
+            "extra": "AMD EPYC 9V74 80-Core Processor · 4 cores · 16GB · linux x64 6.17.0-1018-azure"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Štěpán Granát",
+            "username": "stepan662",
+            "email": "granat.stepan@gmail.com"
+          },
+          "committer": {
+            "name": "Štěpán Granát",
+            "username": "stepan662",
+            "email": "granat.stepan@gmail.com"
+          },
+          "id": "69337debc72ec4acc6845eb83540e2c45e55350c",
+          "message": "chore: update roadmap",
+          "timestamp": "2026-06-21T14:58:10Z",
+          "url": "https://github.com/stepan662/gent-go/commit/69337debc72ec4acc6845eb83540e2c45e55350c"
+        },
+        "date": 1782118675362,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "spawn deep sqlite",
+            "value": 98,
+            "unit": "inst/s",
+            "extra": "AMD EPYC 7763 64-Core Processor · 4 cores · 16GB · linux x64 6.17.0-1018-azure"
+          },
+          {
+            "name": "spawn deep postgres",
+            "value": 279,
+            "unit": "inst/s",
+            "extra": "AMD EPYC 7763 64-Core Processor · 4 cores · 16GB · linux x64 6.17.0-1018-azure"
+          },
+          {
+            "name": "spawn recursive sqlite",
+            "value": 97,
+            "unit": "inst/s",
+            "extra": "AMD EPYC 9V74 80-Core Processor · 4 cores · 16GB · linux x64 6.17.0-1018-azure"
+          },
+          {
+            "name": "spawn recursive postgres",
+            "value": 491,
             "unit": "inst/s",
             "extra": "AMD EPYC 9V74 80-Core Processor · 4 cores · 16GB · linux x64 6.17.0-1018-azure"
           }
